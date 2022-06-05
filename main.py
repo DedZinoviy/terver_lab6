9895762a1b51e8abe9078e716416713852184f10
DmitryDemenkov <demon.krut2002@mail.ru>
2022-06-05 13:10:48 +0300

    Добавлен многострочный ввод из файла

--- main.py
+++ main.py
@@ -478,10 +478,14 @@ class mywindow(QtWidgets.QMainWindow):
 
         try:
             file = open(fileName, 'r', encoding='utf-8')
-            line = file.readline()
+            lines = file.readlines()
+            serias = []
             
-            serias = line.split(' ')
-            serias = [float(var) for var in serias]
+            for line in lines:
+                line = line.strip()
+                line = line.split()
+                for var in line:
+                    serias.append(float(var))
             self.statistic.setSeries(serias)
             
             string_serias = ", ".join(str(var) for var in self.statistic.series)
